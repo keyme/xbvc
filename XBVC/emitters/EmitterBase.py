4b47b0a5b6c7099f95af3bcfb9fb244074747ad7
Lianne Lairmore <lianne.lairmore@key.me>
2018-04-17 15:37:09 -0400

    Changes to run generated python in python 3

--- XBVC/emitters/EmitterBase.py
+++ XBVC/emitters/EmitterBase.py
@@ -1,6 +1,6 @@
 import os.path
 from jinja2 import Environment, FileSystemLoader
-import inspect, os
+import os
 import os.path
 
 class SourceFile(object):
@@ -9,7 +9,7 @@ class SourceFile(object):
         self.name = name
 
     def save_to_disk(self, path):
-        with open(os.path.join(path, self.name), 'wb') as f:
+        with open(os.path.join(path, self.name), 'w') as f:
             f.write(self.content)
 
     def __str__(self):
@@ -24,16 +24,14 @@ class Emitter(object):
     def generate_source(self, commspec, targets):
         pass
 
-    def expand_template(self, template_name, template_vars = {}):
+    def expand_template(self, template_name, template_vars={}):
         base_path = os.path.dirname(__file__)
         tmpl_path = os.path.abspath(os.path.join(base_path, 'templates'))
         env = Environment(loader=FileSystemLoader(tmpl_path))
 
         env.trim_blocks = True
         env.lstrip_blocks = True
-        
+
         tmpl = env.get_template(template_name)
         return tmpl.render(template_vars)
 
-
-        
