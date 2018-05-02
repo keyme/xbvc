a78384ccddf8b9f7ec8d4ca2716c68fe8b8acc80
Jeff Ciesielski <jeff.ciesielski@key.me>
2018-05-01 21:58:23 -0400

    TEST: Add roundtrip test for C
    Ensures that we can encode and decode messages properly

--- test/c_tests/test.c
+++ test/c_tests/test.c
@@ -2,9 +2,15 @@
 #include <stdint.h>
 #include <assert.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include <xbvc_core.h>
 #include <cobs.h>
+#include "gqueue.h"
+
+#define BUFFER_SIZE 512
+DECLARE_QUEUE(buf, char, BUFFER_SIZE);
 
 extern int xbvc_encode_get_command(struct x_get_command *src, uint8_t *dest,
                                    int max_len);
@@ -14,10 +20,17 @@ extern int xbvc_encode_get_response(struct x_get_response *src, uint8_t *dest,
                                     int max_len);
 extern int xbvc_decode_get_response(uint8_t *src, struct x_get_response *dest,
                                     int max_len);
+bool got_heartbeat = false;
+bool got_get = false;
+uint8_t fluffcmp[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 void xbvc_handle_get_command(struct x_get_command *msg)
 {
     XBVCTOUCH(msg);
+    printf("Got Get Command!\n");
+    assert(msg->Target == 0xf00fdada);
+    assert(memcmp(msg->Fluff, fluffcmp, 10) == 0);
+    got_get = true;
 }
 
 void xbvc_handle_get_response(struct x_get_response *msg)
@@ -28,6 +41,9 @@ void xbvc_handle_get_response(struct x_get_response *msg)
 void xbvc_handle_heartbeat(struct x_heartbeat *msg)
 {
     XBVCTOUCH(msg);
+    printf("Got heartbeat!\n");
+    assert(msg->Alive == 0xdeadbeef);
+    got_heartbeat = true;
 }
 
 void xbvc_handle_ping(struct x_ping *msg)
@@ -84,9 +100,77 @@ void test_encode_round_trip(void)
     assert(memcmp(&msg, &decoded_msg, sizeof(struct x_get_response)) == 0);
 }
 
+
+/* This is called by the XBVC state machine */
+static void loopback_init(void *params)
+{
+    XBVCTOUCH(params);
+    queue_init(&buf, sizeof(uint8_t), BUFFER_SIZE);
+}
+
+int loopback_read(uint8_t *dst, int len)
+{
+    for (int i = 0; i < len; i++) {
+        if(queue_is_empty(&buf)) {
+            return i;
+        }
+        queue_dequeue(&buf, dst++);
+    }
+    return len;
+}
+
+int loopback_write(uint8_t *src, int len)
+{
+    for (int i = 0; i < len; i++) {
+        queue_enqueue(&buf, src++);
+    }
+    return len;
+}
+
+void test_roundtrip_encoding(void)
+{
+    xbvc_init(NULL,
+              loopback_read,
+              loopback_write,
+              loopback_init);
+
+    struct x_heartbeat heartbeat;
+    heartbeat.Alive = 0xdeadbeef;
+    xbvc_send(&heartbeat, E_MSG_HEARTBEAT);
+    for(int i = 0; i < 10000; i++) {
+        xbvc_run();
+        if (got_heartbeat) {
+            return;
+        }
+    }
+    assert(false);
+}
+
+void test_roundtrip_encoding2(void)
+{
+    xbvc_init(NULL,
+              loopback_read,
+              loopback_write,
+              loopback_init);
+
+    struct x_get_command getcmd;
+    getcmd.Target = 0xf00fdada;
+    memcpy(fluffcmp, getcmd.Fluff, 10);
+    xbvc_send(&getcmd, E_MSG_GET_COMMAND);
+    for(int i = 0; i < 10000; i++) {
+        xbvc_run();
+        if (got_get) {
+            return;
+        }
+    }
+    assert(false);
+}
+
+
 int main()
 {
     test_encode_round_trip();
-    printf("Hello world!\n");
+    test_roundtrip_encoding();
+    test_roundtrip_encoding2();
     return 0;
 }
