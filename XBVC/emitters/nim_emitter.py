45b015cb6eb9ecceb3c779233ee996a8aa5b10f8
Jeff Ciesielski <jeff.ciesielski@key.me>
2018-11-07 15:37:50 -0500

    CORE: Clean up base objects
    Two changes here:
    1. Move the nim_types stuff into the nim emitter where it belongs
    2. Use a mixin to provide camel/pascal names for objects

--- XBVC/emitters/nim_emitter.py
+++ XBVC/emitters/nim_emitter.py
@@ -3,14 +3,31 @@ from XBVC.emitters.EmitterBase import SourceFile, EmitterBase
 
 EMITTER_NAME = 'nim'
 
+_TYPE_MAP = {
+    'f32': 'float32',
+    'f64': 'float64',
+    'u32': 'uint32',
+    's32': 'int32',
+    'u16': 'uint16',
+    's16': 'int16',
+    'u8': 'uint8',
+    's8': 'int8',
+}
+
 
 class Emitter(EmitterBase):
     def __init__(self):
         super().__init__('Nim')
 
+    def _annotate_messages(self):
+        for msg in self.cs.messages:
+            for member in msg.members:
+                member.nim_d_type = _TYPE_MAP[member.d_type]
+
     def generate_source(self, commspec, targets):
         self.cs = commspec
         source_files = []
+        self._annotate_messages()
         source_files.append(self._generate_interface())
 
         return source_files
