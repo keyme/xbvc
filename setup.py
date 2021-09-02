46acc4cc13c35bce6e34bc1c3114677d24bf35cb
Vaibhav Sagar <vaibhav.sagar@key.me>
2021-09-03 04:08:29 +1000

    setup.py: constrain jinja2 and PyYAML upper bounds

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         ]
     },
     install_requires=[
-        'pyyaml >= 5.3.0',
-        'jinja2 >= 2.10.0',
+        'pyyaml >= 5.3.0, <5.4a0',
+        'jinja2 >= 2.10.0, <3.0.0a0',
     ],
 )
